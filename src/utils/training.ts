d3522c9e167db42203e71cd0b063cf40665c80cd
Ragaeeb Haq <7464039+ragaeeb@users.noreply.github.com>
2025-04-09 02:28:07 -0400

    fix(build): Let bun take care of asset bundling

--- src/utils/training.ts
+++ src/utils/training.ts
@@ -1,14 +1,5 @@
-import { dirname, join } from 'node:path';
-import { fileURLToPath } from 'node:url';
-
 import promptWithFooters from '../../training/prompt_footers.txt' assert { type: 'text' };
 import promptWithoutFooters from '../../training/prompt_no_footers.txt' assert { type: 'text' };
-import samplePageWithFootnotesRelative from '../../training/sample_with_footnotes.jpg' assert { type: 'asset' };
-
-// __dirname of the current module
-const __dirname = dirname(fileURLToPath(import.meta.url));
-
-// Resolve absolute path to the copied asset inside dist/
-const samplePageWithFootnotes = join(__dirname, '..', samplePageWithFootnotesRelative);
+import samplePageWithFootnotes from '../../training/sample_with_footnotes.jpg' assert { type: 'asset' };
 
 export { promptWithFooters, promptWithoutFooters, samplePageWithFootnotes };
